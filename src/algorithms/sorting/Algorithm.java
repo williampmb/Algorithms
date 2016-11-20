3ab1b54b5e4a65c726376250b41ad8066b247e0f
William <William@192.168.0.11>
2016-11-19 21:22:09 -0300

    # QuickSort

---
+++ src/algorithms/sorting/Algorithm.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms.sorting;
+
+/**
+ *
+ * @author William
+ */
+public interface Algorithm {
+    public int[] execute(int[] array);
+}
